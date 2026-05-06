cfbb5e3a0a198bc618e926dce93ea0795cd34e67
AnliouCam <anlioucamara00@gmail.com>
2026-05-06 04:29:54 +0000

    feat: authentification admin avec Laravel Breeze

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\CreneauController;
-use App\Http\Controllers\ReservationController;
 
-Route::get('/', [ServiceController::class, 'index']);
-Route::get('/reserver', [ServiceController::class, 'choisir']);
-Route::get('/reserver/creneaux', [CreneauController::class, 'index']);
-Route::get('/reserver/formulaire', [ReservationController::class, 'create']);
-Route::post('/reserver/formulaire', [ReservationController::class, 'store']);
-Route::get('/reserver/confirmation', [ReservationController::class, 'confirmation']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
